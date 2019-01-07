2a9ce470659ad4647183f684c1b9ac9616dabe58
Lukunin L. Kirill <lukunin.kirill199@gmail.com>
2019-01-08 02:16:36 +0300

    Всё, теперь огонь

--- p1/main.rb
+++ p1/main.rb
@@ -1,8 +1,7 @@
 def files(name)
-  h = File.open('H.txt', 'w')
-  File.read(name).split('a').first
-
-  h.close
+  File.open('H.txt', 'w') do |h|
+    h.write File.read(name).split('a').first
+  end
 rescue StandardError
   puts 'Error opening files'
 end
